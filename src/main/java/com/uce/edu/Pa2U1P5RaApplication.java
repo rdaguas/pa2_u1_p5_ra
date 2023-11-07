d14fb4551c13474f0a9ce217076ca8c5f94cca49
rdaguas <aguas.rdv2001@gmail.com>
2023-11-07 18:55:35 -0500

    Mi primer commit

---
+++ src/main/java/com/uce/edu/Pa2U1P5RaApplication.java
@@ -0,0 +1,21 @@
+package com.uce.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P5RaApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5RaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+		System.out.println("Soy Roberto Aguas");
+	}
+
+}
