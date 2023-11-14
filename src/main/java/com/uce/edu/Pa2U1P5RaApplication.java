48584b85a36d8f2a614cb7e8f60bc5ee5d68637b
rdaguas <aguas.rdv2001@gmail.com>
2023-11-13 19:39:56 -0500

    Taller5

--- src/main/java/com/uce/edu/Pa2U1P5RaApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5RaApplication.java
@@ -1,24 +1,32 @@
 package com.uce.edu;
 
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.uce.edu.ioc.di.Estudiante;
+
 @SpringBootApplication
 public class Pa2U1P5RaApplication implements CommandLineRunner{
 
+	@Autowired
+	private Estudiante estudiante;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5RaApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println("Hola mundo");
-		System.out.println("Soy Roberto Aguas");
-		System.out.println("Cambio en la MISMA rama");
-		System.out.println("Cambio en una NUEVA rama --> Taller 3");
-		System.out.println("Cambio en una NUEVA rama --> Taller 4 version2.0");
+		this.estudiante.setNombre("Roberto");
+		this.estudiante.setApellido("Aguas");
+		this.estudiante.setCedula("1724340391");
+		this.estudiante.setSalario(new BigDecimal(100));
+		System.out.println(estudiante);
+		
 	}
 
 }
