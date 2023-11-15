84837aba7d810f442d99236409c743e5a2394dc3
rdaguas <aguas.rdv2001@gmail.com>
2023-11-14 19:41:12 -0500

    Taller6

---
+++ src/main/java/com/uce/edu/repository/modelo/Materia.java
@@ -0,0 +1,35 @@
+package com.uce.edu.repository.modelo;
+
+public class Materia {
+
+	private String nombre;
+	private String codigo;
+	private Integer numeroCreditos;
+	
+	//get y set
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public Integer getNumeroCreditos() {
+		return numeroCreditos;
+	}
+	public void setNumeroCreditos(Integer numeroCreditos) {
+		this.numeroCreditos = numeroCreditos;
+	}
+	@Override
+	public String toString() {
+		return "Materia [nombre=" + nombre + ", codigo=" + codigo + ", numeroCreditos=" + numeroCreditos + "]";
+	}
+	
+	
+	
+}
